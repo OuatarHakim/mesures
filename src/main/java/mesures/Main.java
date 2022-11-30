6ee3dc45d23779035a1f04f3e46f8890c1ba524a
Hakim ouatar <hakimouatar6@gmail.com>
2022-11-30 09:23:00 +0100

    calcule degré moyen et coefficient de clustering

--- src/main/java/mesures/Main.java
+++ src/main/java/mesures/Main.java
@@ -1,12 +1,14 @@
 package mesures;
 
 
+import org.graphstream.algorithm.Toolkit;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.file.FileSourceEdge;
 
 import javax.imageio.IIOException;
+import java.awt.*;
 import java.io.IOException;
 
 public class Main {
@@ -25,8 +27,18 @@ public class Main {
 
         }
 
+        // mesures de bases
+          afficheMesures(g);
 
 
+
+    }
+
+    public static  void afficheMesures (Graph g){
+        System.out.println("Nombres de nodes : " + g.getNodeCount());
+        System.out.println("Nombres de liens  : " + g.getEdgeCount());
+        System.out.println("Degré Moyen : " + Toolkit.averageDegree(g));
+        System.out.println("Coefficient de clustering : " + Toolkit.clusteringCoefficients(g));
     }
 }
 
