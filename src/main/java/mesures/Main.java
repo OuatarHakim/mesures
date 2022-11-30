7d370f45944f8042fdaee250a1c2fc7ab278affc
Hakim ouatar <hakimouatar6@gmail.com>
2022-11-30 09:10:40 +0100

    read file

--- src/main/java/mesures/Main.java
+++ src/main/java/mesures/Main.java
@@ -1,4 +1,33 @@
 package mesures;
 
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.file.FileSourceEdge;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "data/com-dblp.ungraph.txt";
+        Graph g = new SingleGraph("graphe");
+        FileSourceEdge fs = new FileSourceEdge(true);
+        fs.addSink(g);
+        try {
+            fs.readAll(filePath);
+        } catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            fs.removeSink(g);
+
+        }
+
+
+
+    }
 }
+
+
