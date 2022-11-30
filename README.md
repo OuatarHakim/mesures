6beb5aff4788c26a03b4772d59ae0bee18e7a979
Hakim ouatar <hakimouatar6@gmail.com>
2022-11-30 10:41:48 +0100

    réseau aleatoire de meme degre et meme taille n'est pas connexe

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Mesures
+# Mesures OUATAR Hakim
 
+## 1) Mesures :
+Nombres de nodes : 317080
+Nombres de liens  : 1049866
+Degré Moyen : 6.62208890914917
+## 2) Le coefficient de clusturing pour un réseau aléatoire de la même taille et du même degré moyen  :
+  DegreMoyen  = 
+  N = nombre de noeuds .
+  CoefficientClustering  = DegreMoyen  / N =  2,088459981×10⁻⁵ 
+## 3 ) Le graphe est connexe 
+K : DEGRÉ MOYEN
+Régime connecté : <k> > ln N
+  
--- src/main/java/mesures/Main.java
+++ src/main/java/mesures/Main.java
@@ -3,12 +3,10 @@ package mesures;
 
 import org.graphstream.algorithm.Toolkit;
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.file.FileSourceEdge;
 
-import javax.imageio.IIOException;
-import java.awt.*;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
@@ -35,11 +33,28 @@ public class Main {
     }
 
     public static  void afficheMesures (Graph g){
-        System.out.println("Nombres de nodes : " + g.getNodeCount());
-        System.out.println("Nombres de liens  : " + g.getEdgeCount());
-        System.out.println("Degré Moyen : " + Toolkit.averageDegree(g));
-        System.out.println("Coefficient de clustering : " + Toolkit.clusteringCoefficients(g));
+        int N = g.getNodeCount() ;
+        int nombreArc   = g.getEdgeCount();
+        double degreMoyen =  Toolkit.averageDegree(g);
+        double[] C = Toolkit.clusteringCoefficients(g) ;
+        double CA = degreMoyen /  N ;
+        System.out.println("Nombres de nodes : " + N );
+        System.out.println("Nombres de liens  : " + nombreArc);
+        System.out.println("Degré Moyen : " + degreMoyen);
+        System.out.println("Coefficient de clustering : " + C);
+        System.out.println("Coefficient de clustering d'un Réseau aléatoire : " +  CA);
+        System.out.println("Le graphe est connexe : " + Toolkit.isConnected(g));
+        System.out.println("Un reseau aleatoire de m^ degré moyen  et m^ taile : "  + connecter(degreMoyen,N));
     }
+
+    private static boolean connecter(double d,int N){
+        if(d > Math.log(N)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
 
 
