1f2ca5ee3aadcc082d6aa40a33ef776b18d2dcab
Hakim ouatar <hakimouatar6@gmail.com>
2022-11-30 11:42:19 +0100

    tracer les dd en échelle LogLog

--- src/main/java/mesures/Main.java
+++ src/main/java/mesures/Main.java
@@ -9,7 +9,7 @@ import org.graphstream.stream.file.FileSourceEdge;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+
 import java.util.Locale;
 
 public class Main {
@@ -29,10 +29,14 @@ public class Main {
         }
 
         // mesures de bases
-          afficheMesures(g);
+        afficheMesures(g);
         writeDataFile("./data/lineaire/dd_lineaire.dat",g);
         writeGnuplotFileLineaire("./data/lineaire/dd_lineaire.gnuplot","dd_lineaire.dat", "dd_lineaire.png" ,"dd_lineaire");
         ExcuteCommande("gnuplot", "dd_lineaire.gnuplot", "./data/lineaire/");
+
+        writeDataFile("./data/LogLog/dd_loglog.dat",g);
+        writeGnuplotFileLogLog("./data/LogLog/dd_loglog.gnuplot","dd_loglog.dat", "dd_loglog.png" ,"dd_loglog");
+        ExcuteCommande("gnuplot", "dd_loglog.gnuplot", "./data/LogLog/");
 
     }
 
@@ -69,7 +73,7 @@ public class Main {
             FileWriter fw = new FileWriter(filename);
 
             for (int i = 0; i < dd.length; i++)
-                if (dd[i] !=0) fw.write(String.format(Locale.US, "%6d%20.8f%n", i, (double) dd[i]));
+                if (dd[i] !=0) fw.write(String.format(Locale.US, "%6d%20.8f%n", i, (double) dd[i] /g.getNodeCount()));
             fw.flush();
             fw.close();
         } catch (IOException e) {
@@ -86,6 +90,32 @@ public class Main {
                     + "set ylabel 'p(k)' \n"
                     + "set output '"+GraphPngName+"' \n"
                     + "plot '"+nameData+"' title '"+title+"' with lines ls 1 "
+            );
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeGnuplotFileLogLog(String path,String nameData ,String GraphPngName ,String title){
+        try {
+            FileWriter fw = new FileWriter(path);
+            fw.write("set terminal png  \n"
+                    + "set title \""+title+"\"\n"
+                    + "set xlabel 'k' \n"
+                    + "set ylabel 'p(k)' \n"
+                    + "set output '"+GraphPngName+"' \n"
+                    + "set logscale xy\n"
+                    + "set yrange [1e-6:1]\n"
+
+                    +  "# Poisson\n"
+                    + "lambda = 6.62208890914917\n"
+                    + "poisson(k) = lambda ** k * exp(-lambda) / gamma(k + 1)\n"
+                    + "f(x) = lc - gamma * x  \n"
+                    + "fit f(x) '" + nameData + "' using (log($1)):(log($2)) via lc, gamma \n"
+                    + "c = exp(lc) \n"
+                    +"power(k) = c *k ** (-gamma) \n"
+                    + "plot '"+nameData+"' title '"+title+"', poisson(x) title 'Poisson law' , power(x) title 'Power law'"
             );
             fw.close();
         } catch (IOException e) {
